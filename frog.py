1ccf493293fa1d93e343788bc19e9eb4369f0ca4
Aidan Beckley <aidanbeckley@gmail.com>
2019-09-12 17:39:36 -0500

    gives correct answer now

--- frog.py
+++ frog.py
@@ -13,46 +13,48 @@ from matplotlib import pyplot as plt
 def calculate_probability(pattern, npads):
     
     # create a list of 1s and 0s for jump or no jump at each pad
-    s = str(bin(pattern))
-    l = list(s)[2:]
+    ls = list(str(bin(pattern)))[2:]
     
-    #convert to numbers
-    for k in range(len(l)):
-        l[k] = int(l[k])
+    l = list(range(npads))
+    for k in range(npads):
+        l[k] = 0
+    
+    if (len(ls) < npads):
+        for k in range( npads - len(ls), npads ):
+            l[k] = int(ls[k + len(ls) - npads])
+        
+    else:
+        #convert to numbers
+        for k in range(npads):
+            l[k] = int(ls[k])
         
     #The probability of the first jump (uniform)
-    output = 1.0 / (npads + 1)
+    prob = 1.0
+    remaining = l
+    pad = 0
+    npads_forward = npads
+    i = 0
         
-    #If there is more than one jump...
-    if (l.count(1) != 0):
-        pad = 0
+    if (l.count(1) == 0):
+        prob = 1.0 / (npads + 1)
+    
+    while (remaining.count(1)):
+        prob *= ( 1.0 / (npads_forward + 1) )
+        if ( l[pad + 1:].count(1) == 0 ):
+            break
+            
+        pad += 1
         
-        #find the first jumping point
+        #If we're not at the end, jump to the next visited pad
         while (l[pad] == 0):
             pad += 1
         
-        while (pad < len(l)):
-            npads_forward = npads + 1 - pad
-            
-            #If we're not at the end, jump to the next visited pad
-            # (if one exists)
-            if ( pad < len(l) - 1):
-                pad += 1
-                while (l[pad] == 0 and pad < len(l)-1):
-                    pad += 1
-            #If if does exist, multiply by the probability of landing there
-            if (l[pad]):
-                output *= ( 1.0 / npads_forward )
-            
-            #If it doesn't that means we're at the other end
-            else:
-                break
-            
-            #If the last pad is landed on, break in that case too
-            if ( pad == len(l) - 1 ):
-                break
+        npads_forward = npads - pad
+        remaining = l[pad:]
         
-    return output * (s.count('1') + 1)
+        i += 1
+        
+    return prob * (l.count(1) + 1)
 
 
 def do_the_thing(npads):
@@ -63,13 +65,18 @@ def do_the_thing(npads):
     
     return x
 
+def harmonic(n):
+    x = 0
+    for k in range(1, n + 2):
+        x += 1.0 / k
+    return x
 
 def make_plot(start, end):
     t0 = time.time()
     x = np.array( range(start, end) )
     y = np.zeros(len(x))
     for k in range(len(x)):
-        y[k] = do_the_thing(x[k])
+        y[k] = harmonic(x[k])
     plt.plot(x,y)
     plt.xlabel("number of pads")
     plt.ylabel("average number of jumps")
